ca65938c4a6ca85321f9ebb4f23492137a27045f
Tiago Curado <tiagomcurado@gmail.com>
2025-01-17 14:18:20 +0000

    fix: note reviews

--- Saddle-points.py
+++ Saddle-points.py
@@ -1,12 +1,19 @@
+"""Exercise Saddle points from exercism.
+https://exercism.org/tracks/python/exercises/saddle-points"""
+
 def saddle_points(matrix):
 
     """Saddle points exercise. 
     Detect saddle points in a matrix, being the point on a surface at which the height is maximum in one direction and minimum in another (for example, in the perpendicular direction).
 
-    Parameters: matrix - provides the data as grids that show the heights of the trees. The rows of the grid represent the east-west direction, and the columns represent the north-south direction.
+    Parameters
+    -------- 
+    matrix : list[list[int]] 
+        Provides the data as grids that show the heights of the trees. The rows of the grid represent the east-west direction, and the columns represent the north-south direction.
     
     Returns:
-    candidates - list of dictionary with the position in the matrix of the candidate tree"""
+    candidates : list[dict[str,int]]
+        List of dictionary with the position in the matrix of the candidate tree"""
     
     if not matrix:
         return []  # Return empty list
@@ -20,6 +27,9 @@ def saddle_points(matrix):
     num_rows = len(matrix)
     num_cols = len(matrix[0])
 
+    #Iteraction in the range of the row. Getting the max, stop and pass to line 40, if not, still looking for a max in each row.
+    #Getting to line 40, it will look for every row of that column, checking if it is the minimum number. Getting there, it will append in candidates variable."""
+    
     for i in range(num_rows):
         for j in range(num_cols):
             tree = matrix[i][j]
